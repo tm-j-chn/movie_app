f8ad7fcd266baa3daeaee429e07db85890d54847
tm-j-chn <timchun0212@gmail.com>
2021-07-28 17:26:51 +0900

    before deploy

---
+++ src/Movie.js
@@ -0,0 +1,35 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./Movie.css";
+
+// because the Movie component doesn't have state, we can use function
+function Movie({ id, year, title, summary, poster, genres }) {
+  return (
+    <div className="movie">
+      <img src={poster} alt={title} title={title}></img>
+      <div className="movie__data">
+        <h3 className="movie__title">{title}</h3>
+        <ul className="movie__genres">
+          {genres.map((genre, index) => (
+            <li key={index} className="genres__genre">
+              {genre}
+            </li>
+          ))}
+        </ul>
+        <h5 className="movie__year">{year}</h5>
+        <p className="movie__summary">{summary.slice(0, 250)}...</p>
+      </div>
+    </div>
+  );
+}
+
+Movie.propTypes = {
+  id: PropTypes.number.isRequired,
+  year: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  summary: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
+
+export default Movie;
